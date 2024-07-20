35b6dc18eab0b117e598e39afb3d0e7ab71284e6
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-07-20 12:27:40 +0100

    fix: avatar dropdown menu

--- src/components/layout/AvatarDropdownMenu.tsx
+++ src/components/layout/AvatarDropdownMenu.tsx
@@ -1,20 +1,26 @@
 'use client';
 
-import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import useAuth from '@/hooks/useAuth';
 import { logout } from '@/lib/actions/auth.actions/logout.actions';
 import paths from '@/lib/paths';
+import { getInitials } from '@/lib/utils/utils';
 import { ChevronDown } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
 export default function AvatarDropdownMenu() {
   const router = useRouter();
+  const { user, isLoading } = useAuth();
+  if (!user && !isLoading) router.push(paths.login());
+
+  const initials = user?.name ? getInitials(user?.name) : '**';
 
   const handleLogout = async () => {
     toast.success('You are signed out!');
@@ -25,13 +31,13 @@ export default function AvatarDropdownMenu() {
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-center gap-2">
         <Avatar>
-          {/* <AvatarImage
-          src="https://github.com/shadcn.png"
-          alt="@shadcn"
-        /> */}
-          <AvatarFallback>CN</AvatarFallback>
+          <AvatarImage
+            src={user?.image ? user.image : undefined}
+            alt={`${user?.name}'s avatar`}
+          />
+          <AvatarFallback>{initials}</AvatarFallback>
         </Avatar>
-        <p>User Name</p>
+        <p>{user?.name}</p>
         <ChevronDown size={16} strokeWidth={1} />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -15,3 +15,16 @@ export const typeIdString = (prefix?: string) =>
       message: `Invalid TypeID string${prefix ? ` with prefix ${prefix}` : ''}`,
     },
   );
+
+export function getInitials(name: string): string {
+  const words = name.trim().split(/\s+/);
+
+  if (words.length === 1) {
+    return words[0].slice(0, 2).toUpperCase();
+  }
+
+  const firstInitial = words[0][0];
+  const lastInitial = words[words.length - 1][0];
+
+  return (firstInitial + lastInitial).toUpperCase();
+}
