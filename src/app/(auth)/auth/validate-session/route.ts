8cd2b7214076f99ae33fb604d675bc87637811da
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-07-31 19:26:58 +0100

    refactor: remove unused variables and commented code

--- src/app/(auth)/auth/validate-session/route.ts
+++ src/app/(auth)/auth/validate-session/route.ts
@@ -1,12 +1,5 @@
 import { validateSession } from '@/lib/auth';
-import {
-  ValidateSessionApiResponse,
-  validateSessionClient,
-  ValidateSessionErrorResult,
-  ValidateSessionSuccessResult,
-} from '@/lib/auth/authHelpers';
-import { lucia } from '@/lib/auth/lucia';
-import { errorCatalog } from '@/lib/constants/errorCatalog';
+import { ValidateSessionApiResponse } from '@/lib/auth/authHelpers';
 import { NextRequest, NextResponse } from 'next/server';
 
 export type ValidateSessionApiResult = Promise<
@@ -17,7 +10,6 @@ export async function GET(request: NextRequest): ValidateSessionApiResult {
   const data = await validateSession();
 
   if (!data.isSuccess) {
-    // redirect(paths.login.path);
     return NextResponse.json({
       data,
       success: false,
