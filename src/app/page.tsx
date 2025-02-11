c925d0e2a971144ab01a47e27998612614988230
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-11 22:03:03 +0000

    ♻️ refactor(page): simplify home page component
    - remove unused components and auth logic
    - streamline the page for clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import LoginButton from '@/components/auth/LoginButton'
-import TempComponent from '@/components/temp'
-import { Button } from '@/components/ui/button'
-
 export default function HomePage() {
   return (
     <>
       <h1 className="font-raleway mb-4 text-5xl">I am the home page</h1>
-      {/* <LoginButton mode="redirect"> */}
-      {/* <Button>Login</Button> */}
-      {/* </LoginButton> */}
     </>
   )
 }
