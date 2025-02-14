22510d0eede90043366ecce0dd7eccc631f8a967
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-14 15:35:49 +0000

    ✨ feat(DataReset): add data reset component
    - add DataReset component to allow users to reset data
    - create a collapsible sidebar group for data reset options
    - implement buttons for resetting all data and resetting bookings
    - utilize Radix UI components for collapsible functionality
    - add necessary icons from Lucide React library
    - move related data files and assets to the dataReset feature folder

---
+++ src/features/dataReset/components/DataReset.tsx
@@ -0,0 +1,58 @@
+import { Button } from '@/shared/components/ui/button'
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenuButton,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
+} from '@/shared/components/ui/sidebar'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@radix-ui/react-collapsible'
+import { RefreshCw, ChevronRight } from 'lucide-react'
+
+export default function DataReset() {
+  return (
+    <Collapsible
+      key={'canisss'}
+      title={'canis'}
+      defaultOpen
+      className="group/collapsible"
+    >
+      <SidebarGroup>
+        <SidebarGroupLabel
+          asChild
+          className="group/label text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
+        >
+          <CollapsibleTrigger asChild>
+            <SidebarMenuButton>
+              <RefreshCw />
+              {'Reset Data'}{' '}
+              <ChevronRight
+                size={iconSize}
+                strokeWidth={strokeWidth}
+                className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90"
+              />
+            </SidebarMenuButton>
+          </CollapsibleTrigger>
+        </SidebarGroupLabel>
+        <CollapsibleContent>
+          <SidebarMenuSub className="gap-2">
+            <SidebarMenuSubItem>
+              <SidebarMenuButton asChild>
+                <Button variant="destructive">Reset All</Button>
+              </SidebarMenuButton>
+            </SidebarMenuSubItem>
+            <SidebarMenuSubItem>
+              <SidebarMenuButton asChild>
+                <Button variant="destructive">Reset Bookings</Button>
+              </SidebarMenuButton>
+            </SidebarMenuSubItem>
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </SidebarGroup>
+    </Collapsible>
+  )
+}
