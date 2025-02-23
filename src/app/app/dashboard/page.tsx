5a48e69ea617802c95a1f3ac7aed1fcb0635784d
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-23 17:20:55 +0000

    ✨ feat(dashboard): replace custom filter with ToggleGroup component from shadcn/ui
    - replace `DashboardFilter` and `DashboardFilterButton` components with `ToggleGroup` and `ToggleGroupItem`
    - use `ToggleGroup` for filtering options (Last 7 Days, Last 30 Days, Last 90 Days)
    - set default value to "Last 7 Days"
    - remove unused imports

--- src/app/app/dashboard/page.tsx
+++ src/app/app/dashboard/page.tsx
@@ -1,7 +1,3 @@
-import {
-  DashboardFilter,
-  DashboardFilterButton,
-} from '@/features/dashboard/components/DashboardFilter'
 import DashboardStats from '@/features/dashboard/components/DashboardStats'
 import DashboardSummary from '@/features/dashboard/components/DashboardSummary'
 import SalesChart from '@/features/dashboard/components/SalesChart'
@@ -10,17 +6,21 @@ import {
   PageHeader,
   PageHeaderTitle,
 } from '@/shared/components/ui-custom/PageHeader'
+import {
+  ToggleGroup,
+  ToggleGroupItem,
+} from '@/shared/components/ui/toggle-group'
 
 function DashboardPage() {
   return (
     <div className="grid w-full grid-cols-4 gap-4">
       <PageHeader className="xs:flex-row col-span-4 flex w-full flex-col items-center justify-between gap-4 md:col-span-4">
         <PageHeaderTitle>Dashboard</PageHeaderTitle>
-        <DashboardFilter>
-          <DashboardFilterButton data-active>Last 7 Days</DashboardFilterButton>
-          <DashboardFilterButton>Last 30 Days</DashboardFilterButton>
-          <DashboardFilterButton>Last 90 Days</DashboardFilterButton>
-        </DashboardFilter>
+        <ToggleGroup type="single" defaultValue="Last 7 Days">
+          <ToggleGroupItem value="Last 7 Days">Last 7 Days</ToggleGroupItem>
+          <ToggleGroupItem value="Last 30 Days">Last 30 Days</ToggleGroupItem>
+          <ToggleGroupItem value="Last 90 Days">Last 90 Days</ToggleGroupItem>
+        </ToggleGroup>
       </PageHeader>
       <DashboardStats className="col-span-4" />
       <DashboardSummary className="col-span-4 lg:col-span-2" />
