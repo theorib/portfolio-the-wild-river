ae3de9909fce79aaef9118bb83708d6c03fa3231
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-24 16:43:04 +0000

    ✨ feat(supabase): add Bookings and Booking types
    - add Bookings type by extracting the return type of getBookings function
    - add Booking type by using ArrayElement utility type on Bookings

--- src/services/supabase/supabase.types.ts
+++ src/services/supabase/supabase.types.ts
@@ -1,4 +1,7 @@
+import { type getBookings } from '@/services/supabase/queries/bookings'
 import { type Database } from '@/services/supabase/supabase.auto.types'
+import { type ArrayElement } from '@/shared/types'
 import { type SupabaseClient } from '@supabase/supabase-js'
-
 export type TypedSupabaseClient = SupabaseClient<Database>
+export type Bookings = Awaited<ReturnType<typeof getBookings>>
+export type Booking = ArrayElement<Bookings>
