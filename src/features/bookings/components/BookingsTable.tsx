99e01b548df15b5c9764c316e07932d6d71fa8c8
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-24 13:50:03 +0000

    ♻️ refactor(bookings): rename BookingsTable component and adjust styles
    - rename BookingsTable component to BookingsTable
    - adjust styles of temporary component to improve display on larger screens

--- src/features/bookings/components/BookingsTable.tsx
+++ src/features/bookings/components/BookingsTable.tsx
@@ -5,7 +5,7 @@ import useBookings from '@/features/bookings/hooks/useBookings'
 export default function BookingsTable() {
   const { data: bookings, isLoading } = useBookings()
   return (
-    <pre className="overflow-auto rounded-lg bg-slate-100 p-4">
+    <pre className="max-w-prose overflow-auto rounded-lg bg-slate-100 p-4">
       {JSON.stringify(bookings, null, 2)}
     </pre>
   )
