4773bfee3e3e63b4d40fe7f798121ddd813a9064
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-25 11:53:51 +0000

    💄 style: remove extra space from body class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${objektiv.className} `}>
+      <body className={`${objektiv.className}`}>
         <Providers>
           {children}
           <Toaster />
