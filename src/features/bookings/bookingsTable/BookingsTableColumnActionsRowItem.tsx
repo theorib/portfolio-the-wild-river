e5c90476127f6f74803fc5ba2bde37d29e78e432
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-27 22:30:37 +0000

    ✨ feat(bookings): add booking status handler and check-in/out actions
    - add BookingsStatusSchema and type for booking status validation
    - add BookingStatusHandler type for mapping booking status to actions
    - implement booking status handler logic for check-in and check-out actions
    - add conditional rendering of check-in/out dropdown menu item based on booking status
    - add icons for check-in and check-out actions
    - add onSelect handlers for check-in and check-out actions

--- src/features/bookings/bookingsTable/BookingsTableColumnActionsRowItem.tsx
+++ src/features/bookings/bookingsTable/BookingsTableColumnActionsRowItem.tsx
@@ -1,3 +1,7 @@
+import {
+  BookingsStatusSchema,
+  type BookingsStatus,
+} from '@/features/bookings/schema'
 import { type Booking } from '@/services/supabase/supabase.types'
 import { Button } from '@/shared/components/ui/button'
 import {
@@ -9,17 +13,55 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
 } from '@/shared/components/ui/dropdown-menu'
-import { EllipsisVertical, Eye, LogOut, OctagonX } from 'lucide-react'
+
+import { EllipsisVertical, Eye, LogIn, LogOut, OctagonX } from 'lucide-react'
 import Link from 'next/link'
 
 type BookingListItemDropdownProps = {
   booking: Booking
 }
 
+type BookingStatusHandlerItem = {
+  icon: React.ReactNode | undefined
+  label: string | undefined
+  onClickHandler: (() => void) | undefined
+}
+
+type BookingStatusHandler = Record<BookingsStatus, BookingStatusHandlerItem>
+
 export default function BookingsTableColumnActionsRowItem({
   booking,
 }: BookingListItemDropdownProps) {
   const bookingId = booking.id
+  const { data: bookingStatus, error } = BookingsStatusSchema.safeParse(
+    booking.status,
+  )
+
+  if (!bookingStatus || error) {
+    return null
+  }
+
+  const bookingStatusHandler: BookingStatusHandler = {
+    'checked-out': {
+      icon: undefined,
+      label: undefined,
+      onClickHandler: undefined,
+    },
+    'checked-in': {
+      icon: <LogOut />,
+      label: 'Check out',
+      onClickHandler: () => {
+        console.log('checking out')
+      },
+    },
+    unconfirmed: {
+      icon: <LogIn />,
+      label: 'Check in',
+      onClickHandler: () => {
+        console.log('checking in')
+      },
+    },
+  } as const
 
   return (
     <DropdownMenu>
@@ -40,15 +82,18 @@ export default function BookingsTableColumnActionsRowItem({
               <Eye />
               See Details
             </Link>
-            {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            <LogOut /> Check out
-            {/* <DropdownMenuShortcut>⌘B</DropdownMenuShortcut> */}
-          </DropdownMenuItem>
+          {bookingStatus && bookingStatus !== 'checked-out' ? (
+            <DropdownMenuItem
+              className="flex items-center gap-2"
+              onSelect={bookingStatusHandler[bookingStatus].onClickHandler}
+            >
+              {bookingStatusHandler[bookingStatus].icon}
+              <span>{bookingStatusHandler[bookingStatus].label}</span>
+            </DropdownMenuItem>
+          ) : null}
           <DropdownMenuItem>
             <OctagonX /> Delete
-            {/* <DropdownMenuShortcut>⌘S</DropdownMenuShortcut> */}
           </DropdownMenuItem>
         </DropdownMenuGroup>
       </DropdownMenuContent>
