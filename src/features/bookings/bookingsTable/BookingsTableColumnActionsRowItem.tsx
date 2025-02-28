6645c246d8e3dec107d7566705e81af97cbc04a8
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-28 13:50:01 +0000

    ✨ feat(bookings): prefetch booking data on details menu item hover
    - add prefetch booking query functionality when user hovers over the details menu item
    - add staleTime of 25 seconds to prevent refetching on every hover

--- src/features/bookings/bookingsTable/BookingsTableColumnActionsRowItem.tsx
+++ src/features/bookings/bookingsTable/BookingsTableColumnActionsRowItem.tsx
@@ -1,8 +1,11 @@
+'use client'
+import { bookingQuery } from '@/features/bookings/hooks/useBooking'
 import {
   BookingsStatusSchema,
   type BookingsStatus,
 } from '@/features/bookings/schema'
 import { type Booking } from '@/services/supabase/supabase.types'
+import useSupabaseBrowser from '@/services/supabase/supabaseBrowser'
 import { Button } from '@/shared/components/ui/button'
 import {
   DropdownMenu,
@@ -13,6 +16,7 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
 } from '@/shared/components/ui/dropdown-menu'
+import { useQueryClient } from '@tanstack/react-query'
 
 import { EllipsisVertical, Eye, LogIn, LogOut, OctagonX } from 'lucide-react'
 import Link from 'next/link'
@@ -22,9 +26,10 @@ type BookingListItemDropdownProps = {
 }
 
 type BookingStatusHandlerItem = {
-  icon: React.ReactNode | undefined
-  label: string | undefined
-  onClickHandler: (() => void) | undefined
+  icon?: React.ReactNode | undefined
+  label?: string | undefined
+  onClickHandler?: (() => void) | undefined
+  onHoverHandler?: (() => void) | undefined
 }
 
 type BookingStatusHandler = Record<BookingsStatus, BookingStatusHandlerItem>
@@ -33,20 +38,32 @@ export default function BookingsTableColumnActionsRowItem({
   booking,
 }: BookingListItemDropdownProps) {
   const bookingId = booking.id
+  const queryClient = useQueryClient()
+  const supabaseClient = useSupabaseBrowser()
+
   const { data: bookingStatus, error } = BookingsStatusSchema.safeParse(
     booking.status,
   )
+
+  const prefetchBookingQuery = () => {
+    void (async (): Promise<void> => {
+      await queryClient.prefetchQuery({
+        ...bookingQuery({
+          supabaseClient,
+          bookingId,
+        }),
+        // so we don't prefetch the same query every time the use hovers the item
+        staleTime: 25 * 1000,
+      })
+    })()
+  }
 
   if (!bookingStatus || error) {
     return null
   }
 
   const bookingStatusHandler: BookingStatusHandler = {
-    'checked-out': {
-      icon: undefined,
-      label: undefined,
-      onClickHandler: undefined,
-    },
+    'checked-out': {},
     'checked-in': {
       icon: <LogOut />,
       label: 'Check out',
@@ -74,7 +91,7 @@ export default function BookingsTableColumnActionsRowItem({
         <DropdownMenuLabel>{`Booking #${bookingId}`}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>
+          <DropdownMenuItem onMouseEnter={prefetchBookingQuery}>
             <Link
               href={`bookings/${bookingId}`}
               className="flex items-center gap-2"
