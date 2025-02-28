7cafb8158c92abd261b38e2f2bdfb6437488840b
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-28 14:53:21 +0000

    🐛 fix(bookings): improve booking query to fetch all guest details
    - update the booking query to select all fields from the guestId using '*' for comprehensive guest info

--- src/services/supabase/queries/bookings.ts
+++ src/services/supabase/queries/bookings.ts
@@ -42,7 +42,7 @@ export const getBookingById = async ({
 }) => {
   const { data, error } = await supabaseClient
     .from('bookings')
-    .select(`*, guestId(fullName, id, email), cabinId(name)`)
+    .select(`*, guestId(*), cabinId(name)`)
     .eq('id', bookingId)
     .single()
 
