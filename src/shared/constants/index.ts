1314f4ff54634ccd7c194ece748fb6d9dad08b00
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-02-28 13:52:11 +0000

    ⚡️ perf(constants): reduce default query fetch and stale time intervals
    - decrease default query fetch interval to 5 seconds
    - decrease default query stale time to 5 seconds

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,5 +1,5 @@
 export const SIDEBAR_COOKIE_NAME = 'sidebar_is_open_state'
 export const SIDEBAR_ICON_SIZE = 40
 export const SIDEBAR_ICON_STROKE_WIDTH = 1
-export const DEFAULT_QUERY_FETCH_INTERVAL = 60 * 1000
-export const DEFAULT_QUERY_STALE_TIME = 60 * 1000
+export const DEFAULT_QUERY_FETCH_INTERVAL = 5 * 1000
+export const DEFAULT_QUERY_STALE_TIME = 5 * 1000
