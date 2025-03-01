007eeab3bc3234c5db1bb3c12edacbd446099905
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-03-01 09:03:58 +0000

    ✨ feat(bookings): add BookingDetailsList, BookingDetailsListItem components
    - add BookingDetailsList and BookingDetailsListItem components for displaying booking details
    - update BookingDetails component to use the new components and display booking information
    - add useBookingDates hook to fetch and format booking dates
    - add BookingFlag component to display guest nationality flag
    - add CircleCheck and CircleX icons for breakfast status
    - add Link component for email link
    - format currency and display total price, cabin price, and extras price
    - display booking creation date in the footer

--- src/features/bookings/bookingDetails/BookingDetails.tsx
+++ src/features/bookings/bookingDetails/BookingDetails.tsx
@@ -2,16 +2,54 @@
 
 import BookingDetailsError from '@/features/bookings/bookingDetails/BookingDetailsError'
 import { BookingDetailsSkeleton } from '@/features/bookings/bookingDetails/BookingDetailsSkeleton'
+import BookingFlag from '@/features/bookings/components/BookingFlag'
 import useBooking from '@/features/bookings/hooks/useBooking'
-import { DatesColumnDataSchema } from '@/features/bookings/schema'
+import useBookingDates from '@/features/bookings/hooks/useBookingDates'
+
 import {
   Card,
   CardContent,
+  CardFooter,
   CardHeader,
   CardTitle,
+  // CardHeader,
+  // CardTitle,
 } from '@/shared/components/ui/card'
+import { cn } from '@/shared/lib/utils'
+import { formatCurrency } from '@/shared/lib/utils/helpers'
 import { format } from 'date-fns'
+import { CircleCheck, CircleX, University } from 'lucide-react'
+import Link from 'next/link'
+
 import { useParams } from 'next/navigation'
+
+export const BookingDetailsList = ({
+  className,
+  ...props
+}: React.ComponentProps<'ul'>) => {
+  return (
+    <ul
+      data-slot="booking-details-list"
+      className={cn('flex items-center gap-1', className)}
+      {...props}
+    />
+  )
+}
+export const BookingDetailsListItem = ({
+  className,
+  ...props
+}: React.ComponentProps<'li'>) => {
+  return (
+    <li
+      data-slot="booking-details-list-item"
+      className={cn(
+        'flex items-center gap-1 after:mx-2 after:opacity-50 after:content-["•"] last:after:content-none',
+        className,
+      )}
+      {...props}
+    />
+  )
+}
 
 export default function BookingDetails() {
   const { bookingId } = useParams<{ bookingId: string }>()
@@ -23,43 +61,77 @@ export default function BookingDetails() {
   } = useBooking({
     bookingId: Number(bookingId),
   })
+  const {
+    data: dates,
+    success,
+    error: datesError,
+  } = useBookingDates({
+    startDate: booking?.startDate,
+    endDate: booking?.endDate,
+    numNights: booking?.numNights,
+  })
 
   if (isLoading) return <BookingDetailsSkeleton />
 
-  if (status === 'error') return <BookingDetailsError />
+  if (status === 'error' || datesError) return <BookingDetailsError />
 
-  if (status === 'success' && booking) {
-    // const { data, success, error } = DatesColumnDataSchema.safeParse({
-    //   startDate: booking?.startDate,
-    //   endDate: booking?.endDate,
-    //   numNights: booking?.numNights,
-    // })
-
-    // const formattedStartDate = format(
-    //   new Date(data.startDate || ''),
-    //   'MMM dd yyyy',
-    // )
-
-    // const formattedEndDate = format(new Date(data.endDate || ''), 'MMM dd yyyy')
-
-    // const distance = isToday(startDate)
-    //   ? 'Today'
-    //   : formatDistanceFromNow(startDate)
-    // const stayLength = String(data.numNights)
+  if (status === 'success' && success && booking) {
+    const { startDate, endDate, distance, stayLength } = dates
 
     return (
       <Card>
-        <CardContent>
-          {/* <CardHeader>
-            <CardTitle>Booking #{booking.id}</CardTitle>
-          </CardHeader> */}
-          <h2>
-            {booking.numNights} nights in cabin {booking.cabinId?.name}
-          </h2>
-          <div></div>
-          <div>Name: {booking.guestId?.fullName}</div>
-          <div>{booking.guestId?.email}</div>
+        <CardHeader className="bg-sidebar border-b">
+          <CardTitle className="flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <University />
+              <span>
+                {stayLength} nights in C abin {booking.cabinId?.name}
+              </span>
+            </div>
+            <span>{`${startDate} (${distance}) - ${endDate}`}</span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-6 py-10">
+          <BookingDetailsList>
+            <BookingDetailsListItem>
+              <BookingFlag
+                url={booking?.guestId?.countryFlag}
+                nationality={booking?.guestId?.nationality}
+                flagSize={25}
+              />
+            </BookingDetailsListItem>
+            <BookingDetailsListItem>
+              {booking?.guestId?.fullName}{' '}
+              {booking?.numGuests ? `+ ${booking?.numGuests} guests` : null}
+            </BookingDetailsListItem>
+
+            <BookingDetailsListItem>
+              <Link
+                href={`mailto:${booking?.guestId?.email}`}
+                className="underline underline-offset-3 after:no-underline"
+              >
+                {booking?.guestId?.email}
+              </Link>
+            </BookingDetailsListItem>
+            <BookingDetailsListItem>
+              {booking?.guestId?.nationalID
+                ? `National ID: ${booking?.guestId.nationalID}`
+                : null}
+            </BookingDetailsListItem>
+          </BookingDetailsList>
+
+          <div className="flex items-center gap-2">
+            {booking?.hasBreakfast ? <CircleCheck /> : <CircleX />}
+            {`Breakfast included? ${booking?.hasBreakfast ? 'yes' : 'no'}`}
+          </div>
+          <div className="text-muted bg-muted-foreground rounded-xl p-6">
+            {`Total price: ${formatCurrency(booking.totalPrice || 0)} (${formatCurrency(booking.cabinPrice || 0)} cabin + ${formatCurrency(booking.extrasPrice || 0)} extras)`}
+            <span>{booking.isPaid ? 'Paid' : 'Not paid'}</span>
+          </div>
         </CardContent>
+        <CardFooter className="bg-sidebar border-t pt-6">
+          Booked {format(new Date(booking.created_at), 'PPPPpppp')}
+        </CardFooter>
       </Card>
     )
   }
