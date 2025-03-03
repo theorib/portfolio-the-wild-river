8ec8ec92f725c0689ff3f1c616442c1ff4f1e2d5
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-03-03 19:35:31 +0000

    ♻️ refactor(bookings): create generic pagination schema
    - introduce a generic function `createPaginationSchema` to generate pagination schemas
    - define `PaginationSchema` and `Pagination` types for improved type safety and reusability

--- src/features/bookings/schema.ts
+++ src/features/bookings/schema.ts
@@ -35,3 +35,25 @@ export type BookingsSort = z.output<typeof BookingsSortSchema>
 
 export const BookingsPageSchema = z.number().int().positive()
 export type BookingsPage = z.output<typeof BookingsPageSchema>
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const createPaginationSchema = <TZodObject extends z.ZodObject<any>>(
+  schema: TZodObject,
+) =>
+  z.object({
+    columnName: schema.keyof(),
+    range: z.object({
+      startIndex: z.number(),
+      endIndex: z.number(),
+    }),
+    numberOfItems: z.number(),
+  })
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type PaginationSchema<TZodObject extends z.ZodObject<any>> = ReturnType<
+  typeof createPaginationSchema<TZodObject>
+>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Pagination<TZodObject extends z.ZodObject<any>> = z.infer<
+  PaginationSchema<TZodObject>
+>
