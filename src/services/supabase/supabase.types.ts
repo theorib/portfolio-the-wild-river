34fd60cead03be7738b0af4f60b3d8c9022a6d55
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-03-04 18:28:32 +0000

    ♻️ refactor(supabase): move pagination and sort types to shared types
    - move PaginationLimit, Range, and Sort types from supabase.types.ts to shared/types
    - update type import in supabase.types.ts to reflect the change
    - rename type PaginationLimit to BookingsSearchParams

--- src/services/supabase/supabase.types.ts
+++ src/services/supabase/supabase.types.ts
@@ -1,24 +1,10 @@
 import { type getBookings } from '@/services/supabase/queries/bookings'
 import { type Database } from '@/services/supabase/supabase.auto.types'
-import { type ArrayElement } from '@/shared/types'
+import type { PaginationAndSort, ArrayElement } from '@/shared/types'
 import { type SupabaseClient } from '@supabase/supabase-js'
 export type TypedSupabaseClient = SupabaseClient<Database>
 export type BookingsAutoRow = Database['public']['Tables']['bookings']['Row']
-export type Bookings = Awaited<ReturnType<typeof getBookings>>
+export type Bookings = Awaited<ReturnType<typeof getBookings>>['data']
 export type Booking = ArrayElement<Bookings>
 
-export type Range = {
-  startIndex: number
-  endIndex: number
-}
-
-export type Sort<TData, TKey extends keyof TData = keyof TData> = {
-  columnName: TKey
-  ascending: boolean
-}
-
-export type PaginationLimit<TData, Tkey extends keyof TData = keyof TData> = {
-  numberOfItems: number
-  columnName: Tkey
-  range: Range
-}
+export type BookingsSearchParams = PaginationAndSort<Booking>
