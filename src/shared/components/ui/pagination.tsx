7a48b1f01b9a2a65249c0a059a62b9f6744bc69d
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-03-04 18:30:42 +0000

    ✨ feat(pagination): use Next/Link and improve accessibility
    - update pagination component to use Next/Link for improved navigation
    - enhance accessibility by adding aria-current and aria-disabled attributes
    - adjust tabIndex and pointer-events for better keyboard navigation and user experience

--- src/shared/components/ui/pagination.tsx
+++ src/shared/components/ui/pagination.tsx
@@ -7,6 +7,7 @@ import {
 
 import { cn } from '@/shared/lib/utils'
 import { type Button, buttonVariants } from '@/shared/components/ui/button'
+import Link from 'next/link'
 
 function Pagination({ className, ...props }: React.ComponentProps<'nav'>) {
   return (
@@ -47,17 +48,20 @@ function PaginationLink({
   isActive,
   size = 'icon',
   ...props
-}: PaginationLinkProps) {
+}: PaginationLinkProps & React.ComponentProps<typeof Link>) {
   return (
-    <a
+    <Link
       aria-current={isActive ? 'page' : undefined}
       data-slot="pagination-link"
       data-active={isActive}
+      aria-disabled={!isActive}
+      tabIndex={isActive ? undefined : -1}
       className={cn(
         buttonVariants({
           variant: isActive ? 'outline' : 'ghost',
           size,
         }),
+        isActive ? '' : 'pointer-events-none cursor-not-allowed',
         className,
       )}
       {...props}
