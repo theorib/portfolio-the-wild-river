2e3d943dac68e407f5720d67a3aad836a8e68353
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-04-06 22:22:06 +0100

    ✨ feat(dashboard): implement useTodaysActivities hook

---
+++ src/features/dashboard/hooks/useTodaysActivities.ts
@@ -0,0 +1,26 @@
+import { getBookingsTodaysActivities } from '@/services/supabase/queries/bookings'
+import { type TypedSupabaseClient } from '@/services/supabase/supabase.types'
+import useSupabaseBrowser from '@/services/supabase/supabaseBrowser'
+import { getToday } from '@/shared/lib/utils/helpers'
+import { queryOptions, useQuery } from '@tanstack/react-query'
+
+type TodaysActivitiesQueryProps = {
+  supabaseClient: TypedSupabaseClient
+}
+
+export const todaysActivitiesQuery = ({
+  supabaseClient,
+}: TodaysActivitiesQueryProps) =>
+  queryOptions({
+    // eslint-disable-next-line @tanstack/query/exhaustive-deps
+    queryKey: ['todays-activities', { date: getToday() }],
+    queryFn: () => getBookingsTodaysActivities({ supabaseClient }),
+  })
+
+function useTodaysActivities() {
+  const supabaseClient = useSupabaseBrowser()
+
+  return useQuery(todaysActivitiesQuery({ supabaseClient }))
+}
+
+export default useTodaysActivities
