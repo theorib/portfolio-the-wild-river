ef7738ea3d30fb1d934bdb1077620c4a29c7e654
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-04-06 22:19:22 +0100

    ✨ feat(dashboard): add useBookingsAfterDate hook

---
+++ src/features/dashboard/hooks/useBookingsAfterDate.ts
@@ -0,0 +1,30 @@
+import { getBookingsAfterDate } from '@/services/supabase/queries/bookings'
+import { type TypedSupabaseClient } from '@/services/supabase/supabase.types'
+import useSupabaseBrowser from '@/services/supabase/supabaseBrowser'
+import { getToday } from '@/shared/lib/utils/helpers'
+import { queryOptions, useQuery } from '@tanstack/react-query'
+import { subDays } from 'date-fns'
+
+type BookingsAfterDateQueryProps = {
+  days?: number
+  supabaseClient: TypedSupabaseClient
+}
+
+export const bookingsAfterDateQuery = ({
+  days = 7,
+  supabaseClient,
+}: BookingsAfterDateQueryProps) =>
+  queryOptions({
+    // eslint-disable-next-line @tanstack/query/exhaustive-deps
+    queryKey: ['bookings', { bookingsAfterDate: subDays(getToday(), days) }],
+    queryFn: () =>
+      getBookingsAfterDate({ date: subDays(getToday(), days), supabaseClient }),
+  })
+
+function useBookingsAfterDate(days?: number) {
+  const supabaseClient = useSupabaseBrowser()
+
+  return useQuery(bookingsAfterDateQuery({ days, supabaseClient }))
+}
+
+export default useBookingsAfterDate
