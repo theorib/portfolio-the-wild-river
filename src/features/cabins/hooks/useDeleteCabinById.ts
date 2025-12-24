5c78dcf2e075ec006cfa41161c5bdf2dc620e005
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-12-24 15:05:40 -0300

    ✨ feat(cabins): add useDeleteCabinById hook
    - implement useMutation hook for deleting a cabin by ID
    - invalidate ['cabins'] query on success and show success toast
    - log error and show error toast on failure

---
+++ src/features/cabins/hooks/useDeleteCabinById.ts
@@ -0,0 +1,33 @@
+import logger from '@/features/logger';
+import { deleteCabinById } from '@/services/supabase/mutations/cabins';
+import useSupabaseBrowser from '@/services/supabase/supabaseBrowser';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+type UseCheckDeleteCabinByIdProps = {
+	cabinId: number;
+};
+
+export default function useDeleteCabinById({ cabinId }: UseCheckDeleteCabinByIdProps) {
+	const queryClient = useQueryClient();
+	const supabaseClient = useSupabaseBrowser();
+
+	const useDeleteCabinById = useMutation({
+		mutationKey: ['cabins', { cabinId }, 'delete'],
+		mutationFn: () =>
+			deleteCabinById({
+				supabaseClient,
+				cabinId,
+			}),
+		onSuccess: async () => {
+			await queryClient.invalidateQueries({ queryKey: ['cabins'] });
+			toast.success(`Cabin ${cabinId} successfully deleted`);
+		},
+		onError: (error) => {
+			const message = `Error deleting cabin ${cabinId}`;
+			logger.withError(error).error(message);
+			toast.error(message);
+		},
+	});
+	return useDeleteCabinById;
+}
