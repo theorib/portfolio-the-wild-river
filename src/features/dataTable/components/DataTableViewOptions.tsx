82b6ed0ade1f158f299e51310fcb1d7087b02b03
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-12-26 16:13:11 -0300

    💄 style(dataTable): adjust width of view options dropdown content
    - change dropdown content width from fixed pixel value (150px) to utility class (w-37.5)

--- src/features/dataTable/components/DataTableViewOptions.tsx
+++ src/features/dataTable/components/DataTableViewOptions.tsx
@@ -29,7 +29,7 @@ export function DataTableViewOptions<TData>({ table }: DataTableViewOptionsProps
 					</Button>
 				}
 			/>
-			<DropdownMenuContent align="end" className="w-[150px]">
+			<DropdownMenuContent align="end" className="w-37.5">
 				<DropdownMenuGroup>
 					<DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
 					<DropdownMenuSeparator />
