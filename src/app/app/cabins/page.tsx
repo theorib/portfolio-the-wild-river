ec93f95176fb5fded50aa855958a40bbaaf5065c
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-12-28 20:35:19 -0300

    ♻️ refactor(cabins): update CabinsTable import path
    - change import path for CabinsTable from '@/features/cabins/components/CabinsTable' to '@/features/cabins/components/cabinsTable/CabinsTable'

--- src/app/app/cabins/page.tsx
+++ src/app/app/cabins/page.tsx
@@ -1,4 +1,4 @@
-import CabinsTable from '@/features/cabins/components/CabinsTable';
+import CabinsTable from '@/features/cabins/components/cabinsTable/CabinsTable';
 import { PageHeader, PageHeaderTitle } from '@/shared/components/ui-custom/PageHeader';
 
 export default function CabinsPage() {
