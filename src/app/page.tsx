9de85cf6be7dda610ec75de687895258ba84b11b
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2026-01-15 18:15:55 -0300

    🐛 fix(page): redirect root path to dashboard
    - remove temporary HomePage component
    - redirect '/' route to '/app/dashboard' using next/navigation redirect and next-typesafe-url $path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import { ComponentExample } from '@/components/component-example';
+import { $path } from 'next-typesafe-url';
+import { redirect } from 'next/navigation';
 
 export default function Page() {
-	return <ComponentExample />;
+	redirect($path({ route: '/app/dashboard' }));
 }
