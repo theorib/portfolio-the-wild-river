b8ede29b3e3a299572385c6412f75b67f581e6d3
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2026-03-18 18:30:31 +0000

    ♻️ refactor(tests): remove unused setup code from setupTests.ts - remove import of jest-dom matchers setup - remove mocking of next/image and next/font/google - remove calls to mockNextFont

--- src/lib/testUtils/setupTests.ts
+++ src/lib/testUtils/setupTests.ts
@@ -1,17 +1,5 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-// import '@testing-library/jest-dom/vitest'
-
-import mockNextFont from './mockNextFontGoogle';
-import mockNextImage from './mockNextImage';
-
-mockNextFont(['Rubik_Scribble', 'Inter']);
-
 beforeAll(() => {
-	vi.mock('next/image', () => mockNextImage);
-	vi.mock('next/font/google', () => () => mockNextFont(['Raleway']));
+	// All your beforeAll code here
 });
 
 beforeEach(() => {
